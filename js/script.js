0588dbf40985e40b35d865c2186f89757abc227b
anikroy4 <anikroy.uiu@gmail.com>
2026-07-10 13:26:31 +0600

    footer and banner part added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.sliders').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // autoplay: true,
+
+  autoplaySpeed: 1500,
+  prevArrow: '<i class="fa-solid fa-angle-left leftarrow"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right rightarrow"></i>',
+});
