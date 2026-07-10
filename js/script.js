d083af9145e60e0ff3c88d7459f79e1ec2ead132
anikroy4 <anikroy.uiu@gmail.com>
2026-07-10 19:54:59 +0600

    aboutpart added and footer updated

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ $('.sliders').slick({
   prevArrow: '<i class="fa-solid fa-angle-left leftarrow"></i>',
   nextArrow: '<i class="fa-solid fa-angle-right rightarrow"></i>',
 });
+// Year and time
+
+function dateAndTime() {
+  const now = new Date();
+  
+  document.getElementById('footer-year').textContent = now.getFullYear();
+  
+ 
+  function updateTime() {
+    const clockNow = new Date();
+    document.getElementById('footer-date-time').textContent = clockNow.toLocaleTimeString();
+  }
+  
+  // Run clock immediately and every second
+  updateTime();
+  setInterval(updateTime, 1000);
+}
+ 
+// Run on page load
+document.addEventListener('DOMContentLoaded', dateAndTime);
